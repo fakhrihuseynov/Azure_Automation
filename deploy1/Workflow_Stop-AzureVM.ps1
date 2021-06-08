0911678a4ff95a7584fefda46b75ef73bdf752ee
Fakhri <84241381+fakhrihuseynov@users.noreply.github.com>
2021-06-08 23:45:03 +0300

    Add files via upload

---
+++ deploy1/Workflow_Stop-AzureVM.ps1
@@ -0,0 +1,26 @@
+workflow Shutdown-ARM-VMs-Parallel
+{
+
+	$SubscriptionId         = Get-AutomationVariable -Name  "aeb44919-96b9-42cf-a108-0cec7dc86b31"
+	$TenantID               = Get-AutomationVariable -Name  "42a1205d-ebeb-47bb-bfd5-bcfad63e2353"
+	$CredentialAssetName    = Get-AutomationVariable -Name  "AppRegistrationPassword"
+   
+	"CredentialAssetName: $CredentialAssetName"
+	#Get the credential with the above name from the Automation Asset store
+	$Cred = Get-AutomationPSCredential -Name $CredentialAssetName
+	if(!$Cred) {
+        Throw "Could not find an Automation Credential Asset named '${CredentialAssetName}'. Make sure you have created one in this Automation Account."
+    }
+
+    #Connect to your Azure Account
+	$Account = Login-AzureRmAccount -Credential $Cred -TenantId $TenantID -ServicePrincipal 
+    
+	$subscription = Get-AzureRmSubscription | Where-Object {$_.Id -eq $SubscriptionId} | Set-AzureRmContext
+
+	$vms = Get-AzureRmVM
+		
+	Foreach -Parallel ($vm in $vms){
+	    Write-Output "Stopping $($vm.Name)"
+	    Stop-AzureRmVm -Name $vm.Name -ResourceGroupName $vm.ResourceGroupName -Force
+	}
+}
